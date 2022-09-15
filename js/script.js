f994913eb97d06c0c3462214f630502645c39ea0
Daniel <daniel.jimenezfrancisco@gmail.com>
2022-09-14 21:46:32 -0500

    Added scroll animations.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 'use strict';
+const easyBankInfo = document.getElementById('why-choose-easybank-info')
+const articles = document.getElementById('articles')
 const hamburgerOpen = document.getElementById('hamburger-open')
 const hamburgerClose = document.getElementById('hamburger-close')
 const navMenu = document.getElementById('nav-menu')
@@ -19,3 +21,22 @@ hamburgerClose.addEventListener('click', () =>{
     overlay.style.opacity = '0'
     overlay.style.visibility = 'hidden'
 })
+
+const observer = new IntersectionObserver((entries, observer) =>{
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting)
+        {
+            console.log('Executed')
+            Array.from(entry.target.children).forEach((entry, number) =>{
+                entry.style.transitionDelay = `${(number / 4) - 0.05}s`
+                entry.classList.toggle('active')
+            })
+
+            observer.unobserve(entry.target)
+        }
+    })
+})
+
+observer.observe(easyBankInfo)
+observer.observe(articles)
